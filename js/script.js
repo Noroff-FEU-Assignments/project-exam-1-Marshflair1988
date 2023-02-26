37d2ab473b7709a7fd0c812674b89778befbc5fe
Marshflair1988 <104839692+Marshflair1988@users.noreply.github.com>
2023-02-26 23:51:15 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 
-const form = document.querySelector(".contactForm");
+const form = document.querySelector("#contactForm");
 const fullName = document.querySelector("#fullName");
 const fullNameError = document.querySelector("#fullNameError");
 const email = document.querySelector("#email");
@@ -14,7 +14,45 @@ const messageError = document.querySelector("#messageError");
 function validateForm() {
     event.preventDefault();
 
+    if (checkLength(fullName.value, 5) === true) {
+        fullNameError.style.display = "none";
+    } else {
+        fullNameError.style.display = "block";
+    }
+
+    if (checkLength(email.value, 3) === true) {
+        emailError.style.display = "none";
+    } else {
+        emailError.style.display = "block";
+    }
+
+    if (validateEmail(subject.value) === true) {
+        subjectError.style.display = "none";
+    } else {
+        subjectError.style.display = "block";
+    }
+
+    if (checkLength(message.value) === true) {
+        messageError.style.display = "none";
+    } else {
+        messageError.style.display = "block";
+    }
+
     console.log("hello");
 }
 
-form.addEventListener("submit", validateForm)
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+    if (value.trim().length > len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
