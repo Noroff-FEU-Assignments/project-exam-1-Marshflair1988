7265bea021f3c87afc4d40a497cd4a9909dc22c0
Marshflair1988 <104839692+Marshflair1988@users.noreply.github.com>
2023-06-04 18:50:36 +0200

    trying to get js to work!

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
-const API = "https://good-food-mood.marshwoolgar.no/wp-json/wp/v2/posts"; 
+const homePageRecipes = "https://good-food-mood.marshwoolgar.no/wp-json/wp/v2/posts?";
+
+    const fetchRecipes = async () =>{
+      await fetch(homePageRecipes)
+      .then (response => response.json())
+      .then (data =>{
+    
+    const filterRecipes = data.map (recipe => recipe.recipe); 
 
 
-fetch(API)
-   .then((res) => res.json())
-   .then((data) => {
-       /*Here you write what to do with the data fetched from the api*/
-        })
-   .catch((error) => console.error(error));
+    const recipe = document.getElementById("blog-card");
+    let perRecipe = "";
+    for (let i = 4; i < filterRecipes.length; i++) {
+      perRecipe +=
+        `<div class="blog-card">
+          <figure class="card-banner img-holder" style="--width: 300; --height: 360;">
+            <img src="images/Coconut chicken.jpg" width="300" height="360" loading="lazy" alt="The trick to getting more done is to have the freedom to roam around" class="img-cover">
+          </figure>
+          <div class="card-content">
+            <h3 class="h5">
+              <a href="#" class="card-title hover:underline">
+                <p class="content">${filterRecipes.title[i]}</p>
+              </a>
+            </h3>
+          </div>
+        </div>`;
+    }
 
-   console.log();
+    recipe.innerHTML = perRecipe;
+});
+    }
+
+fetchRecipes();
